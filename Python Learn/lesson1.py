26b90f54afb7ed75939a716aa0efdffa4ad08fd9
Mudzs <98470061+Mudzs@users.noreply.github.com>
2022-01-26 22:16:31 +0800

    Add files via upload

---
+++ Python Learn/lesson1.py
@@ -0,0 +1,42 @@
+# Lesson 1
+
+
+
+print(4)
+print(type(4))
+
+print(4.2)
+print(type(4.2))
+
+print("abdul")
+print(type("a"))
+
+print('aku akan \"menggaggu"')
+
+print(type(True))
+
+print(type(bool(2.2)), bool(2.2))
+print(type(int(13)), int(13))
+print(True, False)
+
+# Varriable
+n = None
+n = 111
+print(n)
+
+## Peraturan Variable 
+# Huruf Kecil Semua, dan Harus mengganti spasi dengan _
+#NumberOfDencending
+#numberOfDencending
+#number_of_decending
+
+# Manipulasi data dengan operator dan expression python
+a = 2
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
